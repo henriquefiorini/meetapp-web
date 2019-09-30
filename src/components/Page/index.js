647812b9a979979ad64543fd95945ca99d60353b
Henrique Fiorini <henriquehfr2@gmail.com>
2019-09-30 09:34:26 -0400

    Simplified Page component

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 
 import { Container, Header, Title, Content } from './styles';
 
-function Page({ title, size, align, children }) {
+function Page({ title, children }) {
   return (
-    <Container size={size} align={align}>
+    <Container>
       {title && (
         <Header>
           <Title>{title}</Title>
@@ -18,8 +18,6 @@ function Page({ title, size, align, children }) {
 
 Page.propTypes = {
   title: PropTypes.string,
-  size: PropTypes.oneOf(['default', 'condensed', 'fullWidth']),
-  align: PropTypes.oneOf(['left', 'center', 'right']),
   children: PropTypes.oneOfType([
     PropTypes.element,
     PropTypes.arrayOf(PropTypes.element),
@@ -28,8 +26,6 @@ Page.propTypes = {
 
 Page.defaultProps = {
   title: null,
-  size: 'default',
-  align: 'left',
 };
 
 export default Page;
